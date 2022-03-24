817e1b19448d469019a816f50f71b071a7a3ae9f
seokjun jin <bbjoony@gmail.com>
2022-03-24 09:00:37 +0900

    changes on 24, March

---
+++ numpy_01.py
@@ -0,0 +1,5 @@
+import numpy as np          # if numpy coundn't be importd properly , change python interpreter another version.
+
+a = np.array([[1, 2, 3, 4, 5], [2, 4, 5, 6, 7], [5, 7, 8, 9, 2]])
+print(a)
+print(a[1][2])              # 2번째 행 / 3번째 열에 있는 값 = '5'
